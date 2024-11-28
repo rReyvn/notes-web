15f8985243714615d75a30e644c6f087dbf056e2
rReyvn <reyvn029@gmail.com>
2024-11-28 20:37:10 +0700

    Create migration, factory, and seeder for notes

---
+++ database/factories/NoteFactory.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NoteFactory extends Factory
+{
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->word(),
+            'content' => fake()->sentence(),
+        ];
+    }
+}
