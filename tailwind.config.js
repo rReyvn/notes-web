87cf79369d339cf1c55f18357871b2f5e843e364
rReyvn <reyvn029@gmail.com>
2024-11-28 20:24:47 +0700

    Add and update module

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
 import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
@@ -16,7 +17,7 @@ export default {
             },
         },
     },
-    plugins: [],
+    plugins: [forms],
 
     darkMode: "selector",
 };
